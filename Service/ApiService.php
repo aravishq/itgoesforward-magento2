c5664f84950afa78216e7cd947b7a23bd9e588e7
Filip Dobrev <filipdbrv@gmail.com>
2025-03-11 17:54:13 +0100

    feat(ip): get IP before getting products

--- Service/ApiService.php
+++ Service/ApiService.php
@@ -38,7 +38,16 @@ class ApiService
     public function getListingByProductId(string $productId): array
     {
         try {
-            $result = $this->getClient()->get("listings/product/{$productId}");
+            if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+                //ip from share internet
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+            }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+                //ip pass from proxy
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }else{
+                $ip = $_SERVER['REMOTE_ADDR'];
+            }
+            $result = $this->getClient()->get("listings/product/{$productId}?ip=$ip");
 
             return json_decode($result->getBody()->getContents(), true);
         } catch (GuzzleException $e) {
